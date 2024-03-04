80a20eb7db71dab6c4329cbad5e1928dccebd45a
Robin DeBank <40622508+ngeeannrobin@users.noreply.github.com>
2024-03-04 21:45:00 +0800

    Implement sendEmail method

--- src/app/network/firebase/notification.service.ts
+++ src/app/network/firebase/notification.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { DocumentReference, Firestore, collection, addDoc, CollectionReference, query, where, collectionData, docData, doc, DocumentData, updateDoc, arrayUnion, arrayRemove, and, deleteDoc, setDoc} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private fs: Firestore) { }
+
+
+  private sendEmail(to: string[], message:any): Promise<void>{
+    let mailCol: CollectionReference = collection(this.fs, "mail");
+    let mailDoc = {
+      to: to,
+      message: message
+    }
+    return new Promise<void>(res=>{
+      addDoc(mailCol, mailDoc).then((docRef: DocumentReference)=>{
+        res();
+      });
+    })
+  }
 }
