bdbe985ae8a2fc184f3b05a21f5cd9334b31d10e
RaeWinTan <raewintan@gmail.com>
2024-03-06 17:44:20 +0800

    make calanderComponent responde to realtime updates of events

--- src/app/common/calander/calander.component.ts
+++ src/app/common/calander/calander.component.ts
@@ -1,8 +1,9 @@
-import { Component, EventEmitter, Input, OnInit, Output, inject} from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, inject} from '@angular/core';
 import { NgbCalendar, NgbDate } from '@ng-bootstrap/ng-bootstrap';
+import { ReplaySubject, take } from 'rxjs';
 import { CalanderEvent } from 'src/app/interfaces/calander-interface/CalanderEvent-interface';
 import { CalanderStatus } from 'src/app/interfaces/enums/calenderenum';
-import { EventInterface } from 'src/app/interfaces/event-interface';
+
 
 
 @Component({
@@ -10,20 +11,29 @@ import { EventInterface } from 'src/app/interfaces/event-interface';
   templateUrl: './calander.component.html',
   styleUrls: ['./calander.component.css']
 })
-export class CalanderComponent implements OnInit{
+export class CalanderComponent implements OnInit, OnChanges{
 	//this is for the person calender page now
 	//just be events here 
-	@Input() events!:CalanderEvent[];
+	@Input() events!:CalanderEvent[];//should have a listernere here so when events refresh will change the clickedDate output
 	@Input() color:CalanderStatus = CalanderStatus.Default;
 	//return the listof event with regards to dates clicked 
 	@Output() clickedDate = new EventEmitter<CalanderEvent[]>();
-
-
+	//here the output will be changed if this calander is for 
+	lastClickedDate = new ReplaySubject<NgbDate>();
   	calendar = inject(NgbCalendar);
 	hoveredDate: NgbDate | null = null;
 	ngOnInit(): void {
 		
 	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if(changes["events"]){
+			this.lastClickedDate.pipe(
+				take(1),
+			).subscribe((d:NgbDate)=>this.onClickDate(d));
+		}
+	}
+
 	isBetween(a:number, b:number, c:number):boolean{
 		return a<= b && b<=c;
 	}
@@ -32,9 +42,10 @@ export class CalanderComponent implements OnInit{
 		return new NgbDate(d.getFullYear(), d.getMonth()+1, d.getDate());
 	}
 	onClickDate(date:NgbDate){
+		//here change 
+		this.lastClickedDate.next(date);
 		this.clickedDate.emit(
 			this.events.filter((x:CalanderEvent)=>{
-				
 				var start:NgbDate = new NgbDate(x.start.getFullYear() , x.start.getMonth()+1,x.start.getDate());
 				var end:NgbDate = new NgbDate(x.end.getFullYear() , x.end.getMonth()+1,x.end.getDate());
 				return this.isBetween(start.year, date.year, end.year) && 
