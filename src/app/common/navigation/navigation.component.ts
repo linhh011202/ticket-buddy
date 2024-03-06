60eb32072aa04b73c4bc6210a8a425b757f15ab0
RaeWinTan <raewintan@gmail.com>
2024-03-06 13:03:35 +0800

    calander stuff

--- src/app/common/navigation/navigation.component.ts
+++ src/app/common/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/network/firebase/authentication.service';
 
 @Component({
   selector: 'app-navigation',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+  constructor(private authApi:AuthenticationService,private router:Router){
 
+  }
+  logout(){
+    this.authApi.logOut();
+    this.router.navigate(["login"]);
+  }
 }
