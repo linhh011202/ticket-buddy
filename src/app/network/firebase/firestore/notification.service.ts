cab8f3a79713f10c480cba01667191a75ae9dfaf
Robin DeBank <40622508+ngeeannrobin@users.noreply.github.com>
2024-04-06 14:19:57 +0800

    implement proper url in email

--- src/app/network/firebase/firestore/notification.service.ts
+++ src/app/network/firebase/firestore/notification.service.ts
@@ -1,3 +1,4 @@
+import { PlatformLocation } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { DocumentReference, Firestore, collection, addDoc, CollectionReference} from '@angular/fire/firestore';
 import { GroupInterface } from 'src/app/interfaces/group-interface';
@@ -7,7 +8,9 @@ import { GroupInterface } from 'src/app/interfaces/group-interface';
 })
 export class NotificationService {
 
-  constructor(private fs: Firestore) { }
+  constructor(
+    private fs: Firestore, 
+    private platformLocation: PlatformLocation) { }
 
 
   private sendEmail(to: string[], message:any): Promise<void>{
@@ -35,10 +38,10 @@ export class NotificationService {
       if (!group.confirmed.includes(member.id))
         emailList.push(member.email);
     });
-
+    var url = (this.platformLocation as any)._location.origin+"/group"+"/"+group.id;
     let content = `
     <p>Hello! ${group.admin.name} is asking for your confirmation to go to <u>${group.event.name||'the concert'}</u> with ${group.name} on ${group.event.startDate!.toDateString()}.</p>
-    <p>Please click <a href='google.com'>here</a> to confirm your availability before ${group.admin.name} books the tickets!</p>
+    <p>Please click <a href='${url}'>here</a> to confirm your availability before ${group.admin.name} books the tickets!</p>
     <p><i>-Ticket Buddy</i></p>
     `
 
